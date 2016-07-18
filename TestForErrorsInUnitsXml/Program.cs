3702cedb1e8e5b99b323a03fd78cef5ffff7e6f6
Michal Maciejewski <Michal_azza@hotmail.com>
2016-07-18 22:30:34 +1000

    Initial

---
+++ TestForErrorsInUnitsXml/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestForErrorsInUnitsXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReaderAndWriter tester = new XmlReaderAndWriter();
+            Console.WriteLine("1.Check All");
+            Console.WriteLine("2.Check by packaging");
+            Console.WriteLine("3.Check Individual");
+            var errorCount = 0;
+            StreamWriter writetext = new StreamWriter("write.txt");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x == 1)
+            {
+                for (char one = 'A'; one <= 'Z'; one++)
+                {
+                    for (char two = 'A'; two <= 'Z'; two++)
+                    {
+                        for (char three = 'A'; three <= 'Z'; three++)
+                        {
+                            for (char four = 'A'; four <= 'Z'; four++)
+                            {
+                                for (char five = 'A'; five <= 'Z'; five++)
+                                {
+                                    for (char six = 'A'; six <= 'Z'; six++)
+                                    {
+                                        for (int count = 0; count < 1000; count++)
+                                        {
+                                            var ifValidSite = true;
+                                            string countNumber ="";
+                                            if(count < 10)
+                                            {
+                                                countNumber = "00" + count.ToString();
+                                            }
+                                            else if(count < 100)
+                                            {
+                                                countNumber = "0" + count.ToString();
+                                            }
+                                            else
+                                            {
+                                                countNumber = count.ToString();
+                                            }
+                                            string builder = one.ToString() + two + three + four + five + six + countNumber;
+                                            string linkOne = "http://training.gov.au/TrainingComponentFiles/" + one + two + three + "/" + builder + "_R1.xml";
+                                            string linkTwo = "http://training.gov.au/TrainingComponentFiles/" + one + two + three + "/" + builder + "_AssessmentRequirements_R1.xml";
+
+                                            Console.WriteLine(builder);
+
+                                            try
+                                            {
+                                                tester.CombiningXmlLinksData(linkOne, linkTwo);
+                                            }
+                                            catch (WebException)
+                                            {
+                                                ifValidSite = false;
+                                            }
+                                            if (ifValidSite)
+                                            {
+                                                try
+                                                {
+                                                    var XmlData = tester.AddXmlDataToUnit(linkOne, linkTwo);
+                                                }
+                                                catch (ArgumentException ex)
+                                                {
+                                                    writetext.WriteLine(builder + "  //---------" + ex.Message + "--------" + ex.InnerException.Message);
+                                                    errorCount++;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(errorCount.ToString());
+                writetext.Close();
+                Console.ReadKey();
+            }
+            else if (x == 2)
+            {                
+                var listOfPackages = new List<string>();
+                var listOfUnitPackages = new List<string>();
+                
+                
+                string[] packages = { "ICT", "SIRX" };
+                string[] unitPackages = { "WHS", "NWK", "ICT", "PRG", "PMG" };
+                for (var count = 0; count < packages.Length; count++)
+                {
+                    listOfPackages.Add(packages[count]);
+                }
+
+                for (var count = 0; count < unitPackages.Length; count++)
+                {
+                    listOfUnitPackages.Add(unitPackages[count]);
+                }
+
+                foreach (var one in listOfPackages)
+                {
+                    foreach (var two in listOfUnitPackages)
+                    {
+                        for (var count = 0; count < 1000; count++)
+                        {
+                            var ifValidSite = true;
+                            string countNumber = "";
+                            if (count < 10)
+                            {
+                                countNumber = "00" + count.ToString();
+                            }
+                            else if (count < 100)
+                            {
+                                countNumber = "0" + count.ToString();
+                            }
+                            else
+                            {
+                                countNumber = count.ToString();
+                            }
+                            string builder = one + two + countNumber;
+                            string linkOne = "http://training.gov.au/TrainingComponentFiles/" + one + "/" + builder + "_R1.xml";
+                            string linkTwo = "http://training.gov.au/TrainingComponentFiles/" + one + "/" + builder + "_AssessmentRequirements_R1.xml";
+
+                            Console.Write(builder);
+
+                            try
+                            {
+                                tester.CombiningXmlLinksData(linkOne, linkTwo);
+                            }
+                            catch (WebException)
+                            {
+                                ifValidSite = false;
+                            }
+                            if (ifValidSite)
+                            {
+                                try
+                                {
+                                    var XmlData = tester.AddXmlDataToUnit(linkOne, linkTwo);
+                                    Console.Write("-Valid");                
+                                }
+                                catch (XmlException ex)
+                                {
+                                    writetext.WriteLine(builder + "  //---------" + ex.Message);
+                                    errorCount++;
+                                    Console.Write("-Invalid");
+                                }
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+                Console.WriteLine(errorCount.ToString());
+                writetext.Close();
+                Console.ReadKey();
+            }
+            else if(x == 3)
+            {
+                var ifValidSite = true;
+                string builder = Console.ReadLine();
+                string linkOne = "http://training.gov.au/TrainingComponentFiles/" + builder.Substring(0, 3) + "/" + builder + "_R1.xml";
+                string linkTwo = "http://training.gov.au/TrainingComponentFiles/" + builder.Substring(0, 3) + "/" + builder + "_AssessmentRequirements_R1.xml";
+
+                Console.Write(builder);
+
+                try
+                {
+                    tester.CombiningXmlLinksData(linkOne, linkTwo);
+                }
+                catch (WebException)
+                {
+                    ifValidSite = false;
+                }
+                if (ifValidSite)
+                {
+                    try
+                    {
+                        var XmlData = tester.AddXmlDataToUnit(linkOne, linkTwo);
+                        Console.Write("-Valid");
+                    }
+                    catch (XmlException ex)
+                    {
+                        writetext.WriteLine(builder + "  //---------" + ex.Message);
+                        errorCount++;
+                        Console.Write("-Invalid");
+                    }
+                }
+                Console.WriteLine();
+                writetext.Close();
+            }
+            else
+            {
+                writetext.Close();
+            }
+        }        
+    }
+}
---
+++ TestForErrorsInUnitsXml/XmlReaderAndWriter.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestForErrorsInUnitsXml
+{
+    public class XmlReaderAndWriter
+    {
+        public List<XmlNode> CombiningXmlLinksData(string xmlCriteria, string xmlAssessment)
+        {
+            var criteriaData = new XmlDocument();
+            var assessmentData = new XmlDocument();
+            criteriaData.Load(xmlCriteria);
+            assessmentData.Load(xmlAssessment);
+            criteriaData["AuthorIT"]["Objects"].InnerXml += assessmentData["AuthorIT"]["Objects"].InnerXml;
+
+            var combinedData = criteriaData.GetElementsByTagName("Topic")
+                .Cast<XmlNode>()
+                .Where(x => x["Object"]["Description"].InnerText.Equals("Elements and Performance Criteria")
+                || x["Object"]["Description"].InnerText.Equals("Performance Evidence")
+                || x["Object"]["Description"].InnerText.Equals("Knowledge Evidence")
+                || x["Object"]["Description"].InnerText.Equals("Assessment Conditions")).ToList();
+
+            return combinedData;
+        }
+
+        public XmlDocument AddXmlDataToUnit(string xmlCriteria, string xmlAssessment)
+        {
+            var topicList = CombiningXmlLinksData(xmlCriteria, xmlAssessment);
+
+            XmlDocument createDocument = new XmlDocument();
+            XmlNode root = createDocument.CreateElement("Matrix");
+            foreach (XmlNode topicNodes in topicList)
+            {
+                //Title of each Element
+                XmlNode headingNodes = createDocument.CreateElement(topicNodes["Object"]["Description"].InnerText.Replace(" ", ""));
+                //Checks for Data inside for each topic
+                XmlNode textNodes = topicNodes["Text"];
+                //Sees if there is any data
+                if (textNodes.ChildNodes.Count >= 1)
+                {
+                    //Checks content of each child elementand then adds it
+                    if (textNodes.FirstChild.Name.Equals("table"))
+                    {
+                        headingNodes.AppendChild(GetTableData(textNodes.FirstChild, createDocument));
+                    }
+                    else
+                    {
+                        //headingNodes.AppendChild(GetListData(textNodes, createDocument));
+                    }
+
+                }
+                root.AppendChild(headingNodes);
+            }
+            createDocument.AppendChild(root);
+            return createDocument;
+        }
+
+        public XmlNode GetTableData(XmlNode textElement, XmlDocument xmlDocument)
+        {
+            XmlNode data = xmlDocument.CreateElement("Data");
+
+            //Each Row
+            foreach (XmlNode row in textElement)
+            {
+
+                if (row != row.ParentNode.FirstChild && row != row.ParentNode.FirstChild.NextSibling)
+                {
+                    XmlNode element = xmlDocument.CreateElement("Elements");
+                    //Each Column                        
+                    foreach (XmlNode column in row)
+                    {
+                        if (column.Name.Equals("td") && !column.InnerText.Equals(""))
+                        {
+                            //Create Element with its list
+                            XmlNode elementTopic = xmlDocument.CreateElement("ElementTopic");
+                            XmlNode criteriaList = xmlDocument.CreateElement("CriteriaList");
+                            //Data Inside the Column
+                            foreach (XmlNode dataInsideColumn in column)
+                            {
+                                //Checks if its in the first column          
+                                if (column == column.ParentNode.FirstChild)
+                                {
+                                    elementTopic.InnerXml = column.InnerXml;
+                                    element.AppendChild(elementTopic);
+                                }
+                                //If not in first column gets all the data inside
+                                else
+                                {
+                                    XmlNode criterion = xmlDocument.CreateElement("Criterion");
+                                    XmlNode performanceCriterionNumber = xmlDocument.CreateElement("PerformanceCriterionNumber");
+                                    XmlNode performanceCriterion = xmlDocument.CreateElement("PerforamceCriterion");
+
+                                    var split = dataInsideColumn.InnerXml.IndexOf(' ');
+                                    performanceCriterionNumber.InnerXml = dataInsideColumn.InnerXml.Substring(0, split);
+                                    performanceCriterion.InnerXml = dataInsideColumn.InnerXml.Substring(split);
+
+                                    criterion.AppendChild(performanceCriterionNumber);
+                                    criterion.AppendChild(performanceCriterion);
+                                    criteriaList.AppendChild(criterion);
+                                    //Checks if its the last list item so it will add list to the topic
+                                    if (dataInsideColumn == column.LastChild && column == column.ParentNode.LastChild)
+                                    {
+                                        element.AppendChild(criteriaList);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    data.AppendChild(element);
+                }
+
+            }
+            return data;
+        }
+
+        public XmlNode GetListData(XmlNode textXml, XmlDocument xmlDocument)
+        {
+            XmlNode data = xmlDocument.CreateElement("Data");
+            var count = 0;
+            var id = "";
+            List<string> listId = new List<string>();
+            var previousId = "";
+            var lists = new List<XmlNode>();
+            //var listData = textXml.ChildNodes.Cast<XmlNode>().Where(p => !p.InnerText.Equals("") && !p.Attributes.GetNamedItem("id").Value.Equals("4")).ToList();
+            var listData = textXml.ChildNodes.Cast<XmlNode>().Where(p => !p.InnerText.Equals("")).ToList();
+            foreach (XmlNode textElement in listData)
+            {
+
+                XmlNode heading = xmlDocument.CreateElement("Heading");
+                if (textElement.Equals(textElement.ParentNode.FirstChild))
+                {
+                    XmlNode list = xmlDocument.CreateElement("List");
+                    lists.Add(list);
+                    previousId = id;
+                    heading.InnerText = textElement.InnerText;
+                    XmlAttribute headingId = xmlDocument.CreateAttribute("id");
+                    //heading.Value = count.ToString();
+                    heading.Attributes.Append(headingId);
+                    id = textElement.Attributes.GetNamedItem("id").Value;
+                    lists[count].AppendChild(heading);
+                    listId.Add(id);
+
+                }
+                else if (textElement.Name.Equals("p") && !textElement.InnerText.Equals(""))
+                {
+                    if (!textElement.Attributes.GetNamedItem("id").Value.Equals(id) && !textElement.NextSibling.Attributes.GetNamedItem("id").Value.Equals(id))
+                    {
+                        count++;
+                        XmlNode list = xmlDocument.CreateElement("List");
+                        lists.Add(list);
+                        previousId = id;
+                        heading.InnerText = textElement.InnerText;
+                        XmlAttribute headingId = xmlDocument.CreateAttribute("id");
+                        heading.Value = count.ToString();
+                        heading.Attributes.Append(headingId);
+                        id = textElement.Attributes.GetNamedItem("id").Value;
+                        lists[count].AppendChild(heading);
+                    }
+                    else if (textElement.Attributes.GetNamedItem("id").Value.Equals("13"))
+                    {
+
+                    }
+
+                }
+            }
+            return null;
+        }
+
+    }
+}
